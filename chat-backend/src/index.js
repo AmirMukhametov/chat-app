b1a777096b08c6982e437e79cb19ea9a8b076201
Amir Mukhametov <ggaben61@gmail.com>
2026-02-21 22:11:03 +0300

    finally fix

--- chat-backend/src/index.js
+++ chat-backend/src/index.js
@@ -1,25 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app, server } from "./lib/socket.js";
+
 import path from "path";
-import { fileURLToPath } from "url";
+
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-// Получаем путь к текущей папке (src) в ES-модулях
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
@@ -32,19 +29,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  // Путь к собранному фронтенду (относительно текущего файла)
-  const frontendDistPath = path.join(__dirname, "../../chat-frontend/dist");
+  app.use(express.static(path.join(__dirname, "../chat-frontend/dist")));
 
-  // Раздача статических файлов
-  app.use(express.static(frontendDistPath));
-
-  // Все остальные GET-запросы отдают index.html (для SPA)
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendDistPath, "index.html"));
+    res.sendFile(path.join(__dirname, "../chat-frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log(`Сервер работает на порту ${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
